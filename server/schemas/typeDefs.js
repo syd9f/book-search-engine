eb7c299bfa4754805ffd4948f34d8c23b19129b3
sydnief <sydniefarrell@gmail.com>
2023-07-05 19:06:10 -0400

    set up schemas folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [bookSchema]!
+  }
+
+  type book_schema {
+    _id: ID
+    authors: String
+    description: String
+    bookId: String
+    image: String,
+    link: String,
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    books(username: String): [Book]
+    book(bookId: ID!): Book
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
